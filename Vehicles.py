c78b6b1445b23579a02e4102f3d1818f5a4e571a
Laght43 <vadimcherkashen@knu.ua>
2025-10-24 21:34:28 +0300

    Improved bugs

--- Vehicles.py
+++ Vehicles.py
@@ -1,13 +1,13 @@
 from Transport import Transport
 
 class Train(Transport):
-    def __init__(self, id, speed, route, price):
+    def __init__(self, id, speed, route=None, price=None):
         super().__init__(id, speed, route, price)
 
 class Bus(Transport):
-    def __init__(self, id, speed, route, price):
+    def __init__(self, id, speed, route=None, price=None):
         super().__init__(id, speed, route, price)
 
 class Airplane(Transport):
-    def __init__(self, id, speed, route, price):
+    def __init__(self, id, speed, route=None, price=None):
         super().__init__(id, speed, route, price)
